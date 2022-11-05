fdb4401866d1c8dca9e0c6318db1e3d7b45a2ea5
yorxika <huamingf@foxmail.com>
2022-11-05 14:42:25 +0800

    feat: leetcode 17.11

--- leetcode/src/Solution.java
+++ leetcode/src/Solution.java
@@ -1,5 +1,8 @@
 public class Solution {
 
+    /**
+     * see [https://leetcode.cn/problems/teemo-attacking/]
+     */
     public int findPoisonedDuration(int[] timeSeries, int duration) {
         int total = 0;
         int last = -1;
@@ -9,6 +12,33 @@ public class Solution {
             last = nextTime;
         }
         return total;
+    }
+
+    /**
+     * see [https://leetcode.cn/problems/find-closest-lcci/]
+     */
+    public int findClosest(String[] words, String word1, String word2) {
+        int last1 = -1;
+        int last2 = -1;
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < words.length; i++) {
+            String cur = words[i];
+            if (cur.equals(word1)) {
+                last1 = i;
+            } else if (cur.equals(word2)) {
+                last2 = i;
+            }
+
+            if (last1 >= 0 && last2 >= 0) {
+                int distance = Math.abs(last2 - last1);
+                result = Math.min(distance, result);
+                if (result == 1) {
+                    return result;
+                }
+            }
+
+        }
+        return result;
     }
 
 }
