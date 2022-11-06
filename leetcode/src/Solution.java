bbad1deff4bf6d107f39f7b07c9ad09739dd47d1
yorxika <huamingf@foxmail.com>
2022-11-06 22:38:43 +0800

    feat: leetcode 17.09

--- leetcode/src/Solution.java
+++ leetcode/src/Solution.java
@@ -58,13 +58,13 @@ public class Solution {
             int cur = arr[i];
             if (i < k) {
                 queue.add(cur);
-            } else if (cur < queue.peek()){
+            } else if (cur < queue.peek()) {
                 queue.poll();
                 queue.add(cur);
             }
         }
         int[] result = new int[k];
-        for (int i = 0; i < k; i ++) {
+        for (int i = 0; i < k; i++) {
             result[i] = queue.poll();
         }
         return result;
@@ -96,6 +96,38 @@ public class Solution {
             }
         }
         return count * 2 > nums.length ? result : -1;
+    }
+
+    /**
+     * see [https://leetcode.cn/problems/get-kth-magic-number-lcci/]
+     */
+    public int getKthMagicNumber(int k) {
+        int l = 1, m = 1, n = 1;
+        int[] arr = new int[k];
+        arr[0] = 1;
+        for (int i = 1; i < arr.length; i++) {
+            int nextL = 3 * arr[l - 1];
+            int nextM = 5 * arr[m - 1];
+            int nextN = 7 * arr[n - 1];
+            int min = Math.min(nextL, Math.min(nextM, nextN));
+            if (min == nextL) {
+                arr[i] = nextL;
+                l++;
+            }
+            if (min == nextM) {
+                arr[i] = nextM;
+                m++;
+            }
+            if (min == nextN) {
+                arr[i] = nextN;
+                n++;
+            }
+        }
+        return arr[k - 1];
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
     }
 
 }
