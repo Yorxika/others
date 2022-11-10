a075d85589e1f6b273ef0db76f54e1c8693ddd65
yorxika <huamingf@foxmail.com>
2022-11-10 23:58:09 +0800

    feat: leetcode 03.04

--- leetcode/src/MyQueue.java
+++ leetcode/src/MyQueue.java
@@ -17,27 +17,31 @@ class MyQueue {
 
     /** Push element x to the back of queue. */
     public void push(int x) {
-        while (!stack.isEmpty()) {
-            queue.push(stack.pop());
-        }
         stack.push(x);
-        while (!queue.isEmpty()) {
-            stack.push(queue.pop());
-        }
     }
 
     /** Removes the element from in front of queue and returns that element. */
     public int pop() {
-        return stack.pop();
+        if (queue.isEmpty()) {
+            while (!stack.isEmpty()) {
+                queue.push(stack.pop());
+            }
+        }
+        return queue.pop();
     }
 
     /** Get the front element. */
     public int peek() {
-        return stack.peek();
+        if (queue.isEmpty()) {
+            while (!stack.isEmpty()) {
+                queue.push(stack.pop());
+            }
+        }
+        return queue.peek();
     }
 
     /** Returns whether the queue is empty. */
     public boolean empty() {
-        return stack.isEmpty();
+        return stack.isEmpty() && queue.isEmpty();
     }
 }
